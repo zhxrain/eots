338dd2c65956d6711c943aba546ed0240f26412d
Zhong Xiang <zhxrain@gmail.com>
2013-10-12 16:58:55 +0800

    Add checkAccess in Role index controller.

--- protected/controllers/RoleController.php
+++ protected/controllers/RoleController.php
@@ -15,18 +15,33 @@ class RoleController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-    $criteria=new CDbCriteria;
-    $criteria->addCondition("type=2"); 
-    $dataProvider =new CActiveDataProvider('Role', array(
-      'criteria'=>$criteria,
-      'pagination'=>array(
-        'pageSize'=>10,
-        'pageVar'=>'page',
-      ),
-    ));
-    $this->render('index',array(
-      'dataProvider'=>$dataProvider,
-    ));
+    //$auth=Yii::app()->authManager;
+    //$role = $auth->createRole('gen');
+    //$auth->createOperation('showRoles','show roles');
+    //$role->addChild('showRoles');
+    //$auth->assign('gen','admin');
+    if(Yii::app()->user->checkAccess('showRoles')){
+      $auth=Yii::app()->authManager;
+      $op = $auth->getOperations();
+      if(!isset($op['showRoles'])){
+        $auth->createOperation('showRoles','show roles');
+        $auth->save();
+      }
+      $criteria=new CDbCriteria;
+      $criteria->addCondition("type=2"); 
+      $dataProvider =new CActiveDataProvider('Role', array(
+        'criteria'=>$criteria,
+        'pagination'=>array(
+          'pageSize'=>10,
+          'pageVar'=>'page',
+        ),
+      ));
+      $this->render('index',array(
+        'dataProvider'=>$dataProvider,
+      ));
+      return;
+    }
+    $this->render('error');
   }
 }
 
