f7191887d8ee628115696f3af9b7e130bd1da1b3
Zhong Xiang <zhxrain@gmail.com>
2013-11-05 17:13:36 +0800

    Add auth.

--- protected/controllers/AccountController.php
+++ protected/controllers/AccountController.php
@@ -15,18 +15,22 @@ class AccountController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-    $dataProvider =new CActiveDataProvider('User', array(
-      'pagination'=>array(
-        'pageSize'=>10,
-        'pageVar'=>'page',
-      ),
-      'sort'=>array(
-        'defaultOrder'=>'id',
-      ),
-    ));
-    $this->render('index',array(
-      'dataProvider'=>$dataProvider,
-    ));
+    if(Yii::app()->user->checkAccess('showUsers')){
+      $dataProvider =new CActiveDataProvider('User', array(
+        'pagination'=>array(
+          'pageSize'=>10,
+          'pageVar'=>'page',
+        ),
+        'sort'=>array(
+          'defaultOrder'=>'id',
+        ),
+      ));
+      $this->render('index',array(
+        'dataProvider'=>$dataProvider,
+      ));
+      return;
+    }
+    $this->render("/site/authError");
   }
 }
 
