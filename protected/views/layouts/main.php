e38fae3a4fa24cbe9a0db610094352c15fb903f4
Zhong Xiang <zhxrain@gmail.com>
2013-11-06 17:02:21 +0800

    Delete header and footer.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -21,11 +21,6 @@
 <body>
 
 <div class="container" id="page">
-
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
-
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
@@ -46,12 +41,6 @@
 	<?php echo $content; ?>
 
 	<div class="clear"></div>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
 
 </div><!-- page -->
 
