0d41b74246746a6a992734f6db7f8db69b15b7e2
zhonghao <zhonghao@zhonghao-OptiPlex-360>
2013-11-07 17:16:57 +0800

    Simple layout with nav sidebar.

--- protected/controllers/RoleController.php
+++ protected/controllers/RoleController.php
@@ -3,18 +3,19 @@
 class RoleController extends Controller
 {
   /**
-	 * Declares class-based actions.
-	 */
-	
+   * Declares class-based actions.
+   */
+
 
   /**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
-	public function actionIndex()
-	{
-		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
+   * This is the default 'index' action that is invoked
+   * when an action is not explicitly requested by users.
+   */
+  public function actionIndex()
+  {
+    $this->layout = 'page';
+    // renders the view file 'protected/views/site/index.php'
+    // using the default layout 'protected/views/layouts/main.php'
     //$auth=Yii::app()->authManager;
     //$role = $auth->createRole('gen');
     //$auth->createOperation('showRoles','show roles');
